9b133ab30ec3e833dee4f4e0595db2c3241a643c
José Valenzuela <josecinuwu@gmail.com>
2025-10-30 19:48:13 -0700

    .

---
+++ functions/api.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+  const data = [
+    { id: 1, nombre: "Damian Lescas", materia: "PWA", calificacion: 95 },
+    { id: 2, nombre: "Arturo Zamorano", materia: "PWA", calificacion: 88 },
+    { id: 3, nombre: "Luis Vega", materia: "PWA", calificacion: 90 }
+  ];
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  };
+};
